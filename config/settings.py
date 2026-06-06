a22d21c7f41ba494c8bcd31bc807818a029fb3d4
ramazon07-cmd <ustau2453@gmail.com>
2026-06-06 22:47:13 +0500

    settings changed

--- config/settings.py
+++ config/settings.py
@@ -165,6 +165,8 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # CORS
+CORS_ALLOW_ALL_ORIGINS = os.environ.get('CORS_ALLOW_ALL_ORIGINS', 'False') == 'True'
+
 CORS_ALLOWED_ORIGINS = os.environ.get(
     'CORS_ALLOWED_ORIGINS',
     'http://localhost:3000,http://127.0.0.1:3000'
